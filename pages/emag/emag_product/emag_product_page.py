7579df1831c4a9225556fd88a777d587de5fabe5
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2024-07-30 01:54:11 +0300

    feat(emag-product): create a product page and added var for product price

---
+++ pages/emag/emag_product/emag_product_page.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page, expect
+from shared.base_page import BasePage
+from shared.get_products import get_products_from_excel, add_product_data
+
+from pages.emag.emag_product.emag_product_page_locators import EmagProductPageLocators
+
+class EmagProductPage(BasePage):
+    URL = '/'
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+    
+    def load(self) -> None:
+        self.page.wait_for_load_state('networkidle')
+        self.emag_product_price = self.page.locator(EmagProductPageLocators.EMAG_PRODUCT_PRICE)
