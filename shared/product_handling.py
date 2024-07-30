576c5fe4f291d38b94a88185a03e781bfb4a46bf
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2024-07-31 01:05:38 +0300

    feat(product-handling): added a function to write in excel file some product data

--- shared/product_handling.py
+++ shared/product_handling.py
@@ -7,13 +7,14 @@ def get_products_from_excel(file_path):
     products = pd.Series(df.iloc[:, 1].values, index=df.iloc[:, 0]).to_dict()
     return products
 
-def add_product_data(file_path): 
+def add_product_data(file_path:str, product_name: str, product_price: str, date_time: str): 
     
-    df = pd.read_excel(file_path)
     wb = load_workbook(file_path)
     ws = wb.active
     last_row = ws.max_row + 1
-    ws[f'A{last_row}'] = 'Test-Text'
+    ws[f'A{last_row}'] = product_name
+    ws[f'B{last_row}'] = product_price
+    ws[f'C{last_row}'] = date_time
 
     wb.save(file_path)
     
