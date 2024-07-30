ed77759365b172a12afbcb2c830807e48798caed
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2024-07-31 01:07:01 +0300

    feat(test-emag-product): added a skeleton test, which check products from excel, browse in emag for these collect price and add in another excel file

--- tests/emag/emag-product/test_emag_product_page.py
+++ tests/emag/emag-product/test_emag_product_page.py
@@ -3,3 +3,14 @@ from pages.emag.emag_product.emag_product_page import EmagProductPage
 def test_emag_product_page(emag_product_page: EmagProductPage):
     
     emag_product_page.load()
+    products = emag_product_page.get_products_from_source('assets/products.xlsx')
+    for product_name, product_url in products.items():
+        emag_product_page.browse_products(product_url)
+        product_price = emag_product_page.get_product_price()
+        emag_product_page.add_product_data_in_db(product_name, product_price)
+
+
+#TODO - BUG
+#TODO: Fix the human verification. 
+#TODO: Fix when stock for a product is < 3, and price label is different.
+#TODO: Fix write bug, if you delete lines in 'price_logs' and script write in file, this is start from last text line, before deletion.
