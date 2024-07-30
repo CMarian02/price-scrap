3f734a103ec8156747cd37ab5943ef2a9e0a6a8b
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2024-07-31 00:43:58 +0300

    feat(emag-product-page): added functions to get products, browse in this and get price.

--- pages/emag/emag_product/emag_product_page.py
+++ pages/emag/emag_product/emag_product_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, expect
 from shared.base_page import BasePage
 from shared.product_handling import get_products_from_excel, add_product_data
+from datetime import datetime
 
 from pages.emag.emag_product.emag_product_page_locators import EmagProductPageLocators
 
@@ -13,3 +14,15 @@ class EmagProductPage(BasePage):
     def load(self) -> None:
         self.page.wait_for_load_state('networkidle')
         self.emag_product_price = self.page.locator(EmagProductPageLocators.EMAG_PRODUCT_PRICE)
+
+    def get_products_from_source(self, file_source: str) -> list:
+        return get_products_from_excel(file_source)
+    
+    def browse_products(self, product_url: str) -> None:
+        self.page.goto(product_url)
+    
+    def get_product_price(self) -> str:
+        return self.emag_product_price.text_content()
+    
+    def add_product_in_db(self, product_name: str, product_price: str):
+        add_product_data('assets/prices_log.xlsx', product_name, product_price, datetime.now())
